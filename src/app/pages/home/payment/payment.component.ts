e1f1c275635767ccca6f5ade79545059e2d1b411
Fightlite <Fightlite@github.com>
2022-01-17 22:11:02 +0700

    update again

--- src/app/pages/home/payment/payment.component.ts
+++ src/app/pages/home/payment/payment.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payment.component.css']
 })
 export class PaymentComponent implements OnInit {
+  // this component receive data by @Output and EventEmitter from child components
 
   constructor() { }
 
